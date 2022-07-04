af0f5e5a5f5ddc7574f787b0ad85dcbc71452d6e
bia9400 <ferro.biagio@gmail.com>
2022-07-04 11:26:58 +0200

    recent news done

--- src/components/RecentNews.vue
+++ src/components/RecentNews.vue
@@ -3,25 +3,25 @@
     <div class="container">
       <div class="d-flex justify-content-between px-3">
         <h2>RECENT NEWS</h2>
-        <span>Read All News</span>
+        <span class="orangespan">Read All News</span>
       </div>
       <div class="row">
-        <div class="col" v-for="speaker in newses" :key="speaker.img">
+        <div class="col" v-for="(speaker,i) in newses" :key="speaker.img">
           <div class="card py-5" style="width: 18rem">
             <div class="card-body">
-              <img :src="`img/speakers/${speaker.img}`" alt="" />
+              <img :src="`img/newsImg/news${i+1}.jpg`" alt="" />
 
-              <p class="card-text">
+              <p class="card-text mt-4 pb-2">
                 <strong>{{ speaker.description.job }}</strong>
               </p>
-              <h6 class="card-title">
+              <h5 class="card-title">
                 <strong>{{ speaker.description.name }}</strong>
-              </h6>
+              </h5>
 
-              <p class="card-text">
+              <p class="card-text pb-3">
                 <strong>{{ speaker.description.theme }}</strong>
               </p>
-              <a href="#" class="card-link"><strong>{{speaker.description.link}}</strong> </a>
+              <a href="#" class="card-link">{{speaker.description.link}} </a>
             </div>
           </div>
         </div>
@@ -40,8 +40,8 @@ export default {
         {
           img: "speaker-6.jpg",
           description: {
-            name: "PATRICK SPANCER",
-            job: "  S&P Analizer",
+            name: "SEDIAL EIUSMOD TEMPOR",
+            job: "03 Dec 2013  /  0 Comments",
             theme:
               " Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur accusamus repudiandae cumque amet, enim tempore sunt. Fugiat, expedita esse sequi pariatur nulla repudiand...",
               link:"Read More"
@@ -51,8 +51,8 @@ export default {
         {
           img: "speaker-5.jpg",
           description: {
-            name: "JANET JONES",
-            job: "New York Post's GM",
+            name: "DONEC LUCTUS IMPERDIET",
+            job: "03 Dec 2013  /  2 Comments",
             theme:
               " Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur accusamus repudiandae cumque amet, enim tempore sunt. Fugiat, expedita esse sequi pariatur nulla repudiand...",
               link:"Read More"
@@ -61,8 +61,8 @@ export default {
         {
           img: "speaker-4.jpg",
           description: {
-            name: "MICHAEL DOVER",
-            job: "Starbuck's CEO",
+            name: "MAGNA PARS STUDIORUM",
+            job: "03 Dec 2013  /  2 Comments",
             theme:
               " Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur accusamus repudiandae cumque amet, enim tempore sunt. Fugiat, expedita esse sequi pariatur nulla repudiand...",
               link:"Read More"
@@ -82,6 +82,16 @@ export default {
   }
   img {
     width: 100%;
+  }
+  p{
+    margin: 0;
+    font-size: 1rem;
+  }
+  a{
+    font-style: italic;
+  }
+  .orangespan{
+    color: #f62b0a;
   }
 }
 </style>
